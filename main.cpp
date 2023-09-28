20f95c3ccf793405cf5a853dcca6ebd88483fda9
Bryan Perfetto <regularkid@gmail.com>
2023-09-28 08:15:14 -0700

    Scratch allocator now uses templates to automatically determine size + alignment of type allocations.

--- main.cpp
+++ main.cpp
@@ -64,25 +64,15 @@ public:
         m_cur = nullptr;
     }
 
-    static void* allocate(const size_t numBytes)
+    static void* allocate(const size_t numBytes, const size_t alignment)
     {
         if (numBytes == 0)
         {
             return nullptr;
         }
 
-        printf("scratchAllocator::allocate\n");
+        printf("scratchAllocator::allocate numBytes: %llu, alignment: %llu\n", numBytes,  alignment);
         init();
-
-        size_t alignment = 8;
-        switch (numBytes)
-        {
-            case 1: alignment = 1; break;
-            case 2: alignment = 2; break;
-            case 3: alignment = 4; break;
-            case 4: alignment = 4; break;
-            default: alignment = 8;
-        }
 
         size_t curPos = (size_t)m_cur;
         size_t newPos = curPos & ~(alignment - 1);
@@ -94,6 +84,17 @@ public:
         char* newPtr = m_cur;
         m_cur += numBytes;
         return newPtr;
+    }
+
+    static void* allocate(const size_t numBytes)
+    {
+        return allocate(numBytes, 8);   // Default to 8 byte alignment
+    }
+
+    template <class T>
+    static T* allocate()
+    {
+        return static_cast<T*>(allocate(sizeof(T), alignof(T)));
     }
 
     static void reset()
@@ -123,37 +124,54 @@ public:
     {
         //allocator::deallocate(ptr);
     }
+
+public:
+    char m_c = 0;
+    short m_s = 0;
+    int m_i = 0;
+    long long m_l = 0;
+    void* m_p = nullptr;
 };
 
 int main()
 {
     printf("----- allocators -----\n");
 
-    char* c = (char*)scratchAllocator::allocate(sizeof(char));
+    char* c = scratchAllocator::allocate<char>();
     printf("c : 0x%p\n", (void*)c);
 
-    int* i = (int*)scratchAllocator::allocate(sizeof(int));
+    int* i = scratchAllocator::allocate<int>();
     printf("i : 0x%p\n", (void*)i);
 
-    short* s = (short*)scratchAllocator::allocate(sizeof(short));
+    short* s = scratchAllocator::allocate<short>();
     printf("s : 0x%p\n", (void*)s);
 
-    long long* l = (long long*)scratchAllocator::allocate(sizeof(long long));
+    long long* l = scratchAllocator::allocate<long long>();
     printf("l : 0x%p\n", (void*)l);
+
+    thing* t = scratchAllocator::allocate<thing>();
+    printf("t : 0x%p\n", (void*)t);
+
+    thing* t2 = new thing();
+    printf("t2 : 0x%p\n", (void*)t2);
 
     *c = 1;
     *s = 2;
     *i = 3;
     *l = 4;
+    t->m_i = 123;
+    t2->m_i = 456;
 
     printf("c : %d\n", *c);
     printf("s : %d\n", *s);
     printf("i : %d\n", *i);
     printf("l : %lld\n", *l);
+    printf("t->m_i : %d\n", t->m_i);
+    printf("t2->m_i : %d\n", t2->m_i);
 
     scratchAllocator::reset();
 
-    char* c2 = (char*)scratchAllocator::allocate(sizeof(char));
+    char* c2 = scratchAllocator::allocate<char>();
     printf("c2 : 0x%p\n", (void*)c2);
 
     *c2 = 5;
