8f3c951bda2f16d0bfc4516da687f3eadd8d06e8
Bryan Perfetto <regularkid@gmail.com>
2023-09-27 20:00:46 -0700

    Quick scratch allocator.

--- main.cpp
+++ main.cpp
@@ -44,21 +44,51 @@ void operator delete[](void* ptr, size_t numBytes)
 }
 
 // -----------------------------------------------------------------------------
-class allocator
+class scratchAllocator
 {
 public:
-    static void* allocate(const size_t numBytes)
+    static void init()
     {
-        printf("allcator::allocate\n");
-        return malloc(numBytes);
+        if (!m_base)
+        {
+            const size_t maxSize = 1024 * 1024;  // 1mb
+            m_base = (char*)malloc(maxSize);
+            m_cur = m_base;
+        }
     }
 
-    static void deallocate(void* ptr)
+    static void shutdown()
     {
-        printf("allocator::deallocate\n");
-        free(ptr);
+        delete m_base;
+        m_base = nullptr;
+        m_cur = nullptr;
     }
+
+    static void* allocate(const size_t numBytes)
+    {
+        printf("scratchAllocator::allocate\n");
+        init();
+
+        const size_t alignment = 8; // Temp - just always use 8 byte alignment
+
+        char* newPtr = m_cur;
+        m_cur += (numBytes + alignment) & ~0x7;
+        return newPtr;
+    }
+
+    static void reset()
+    {
+        printf("scratchAllocator::reset\n");
+        m_cur = m_base;
+    }
+
+private:
+    static char* m_base;
+    static char* m_cur;
 };
+
+char* scratchAllocator::m_base = nullptr;
+char* scratchAllocator::m_cur = nullptr;
 
 // -----------------------------------------------------------------------------
 class thing
@@ -66,12 +96,12 @@ class thing
 public:
     void* operator new(size_t numBytes)
     {
-        return allocator::allocate(numBytes);
+        return scratchAllocator::allocate(numBytes);
     }
 
     void operator delete(void* ptr)
     {
-        allocator::deallocate(ptr);
+        //allocator::deallocate(ptr);
     }
 };
 
@@ -79,14 +109,35 @@ int main()
 {
     printf("----- allocators -----\n");
 
-    int* a = new int;
-    delete a;
+    char* c = (char*)scratchAllocator::allocate(sizeof(char));
+    printf("c : 0x%p\n", (void*)c);
 
-    int* b = new int[5];
-    delete[] b;
+    short* s = (short*)scratchAllocator::allocate(sizeof(short));
+    printf("s : 0x%p\n", (void*)s);
 
-    thing* t = new thing();
-    delete t;
+    int* i = (int*)scratchAllocator::allocate(sizeof(int));
+    printf("s : 0x%p\n", (void*)i);
+
+    long long* l = (long long*)scratchAllocator::allocate(sizeof(long long));
+    printf("l : 0x%p\n", (void*)l);
+
+    *c = 1;
+    *s = 2;
+    *i = 3;
+    *l = 4;
+
+    printf("c : %d\n", *c);
+    printf("s : %d\n", *s);
+    printf("i : %d\n", *i);
+    printf("l : %lld\n", *l);
+
+    scratchAllocator::reset();
+
+    char* c2 = (char*)scratchAllocator::allocate(sizeof(char));
+    printf("c2 : 0x%p\n", (void*)c2);
+
+    *c2 = 5;
+    printf("c2 : %d\n", *c2);
 
     return 0;
 }
